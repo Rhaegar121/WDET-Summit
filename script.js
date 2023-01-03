90815015ae853d268daf688e5a80c65eac9842b8
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-01-03 14:41:33 +0630

    add about page for mobile version

---
+++ script.js
@@ -0,0 +1,17 @@
+const bar = document.querySelector('#mobile-nav');
+const menu = document.querySelector('#mobile-menu');
+const xmark = document.querySelector('#xmark');
+
+function mobileMenu() {
+    if(menu.style.display === 'block') {
+        menu.style.display = 'none';
+    } else menu.style.display = 'block';
+}
+
+bar.addEventListener('click', mobileMenu);
+menu.addEventListener('click', mobileMenu);
+xmark.addEventListener('click', mobileMenu);
+// xmark.onclick = () => {
+//     menu.style.display = 'none';
+//     console.log('dkdf');
+// }
