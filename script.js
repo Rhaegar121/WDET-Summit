13e92e473a57b2ac05cf0eb22eb281277a24f4d7
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-01-03 17:22:06 +0630

    add mobile menu for the mobile version

--- script.js
+++ script.js
@@ -3,11 +3,19 @@ const menu = document.querySelector('#mobile-menu');
 const xmark = document.querySelector('#xmark');
 
 function mobileMenu() {
-  if (menu.style.display === 'block') {
+  if (menu.style.display === 'block' && bar.style.display === 'none') {
     menu.style.display = 'none';
-  } else menu.style.display = 'block';
+    bar.style.display = 'flex';
+  } else {
+    menu.style.display = 'block';
+    bar.style.display = 'none';
+  }
 }
 
 bar.addEventListener('click', mobileMenu);
 menu.addEventListener('click', mobileMenu);
-xmark.addEventListener('click', mobileMenu);
+// xmark.addEventListener('click', mobileMenu);
+xmark.onclick = () => {
+    menu.style.display = 'block';
+    bar.style.display = 'none';
+}
